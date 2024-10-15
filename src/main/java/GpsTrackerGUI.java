be1ed4a9179bbd1544844aa8720f5772a23150a8
jesse <1152122933@qq.com>
2024-10-15 15:50:46 +1030

    finish program initial skeleton

---
+++ src/main/java/GpsTrackerGUI.java
@@ -0,0 +1,10 @@
+import nz.sodium.Stream;
+
+/**
+ *
+ */
+public class GpsTrackerGUI {
+    public GpsTrackerGUI(Stream<GpsEvent>[] gpsStreams) {}
+    public void createGUI() {}
+    public void processGpsStreams() {}
+}
